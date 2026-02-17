b3041fac91b03a74e98dbfd0852d25a41f3914d3
QGrain <zhiyuzhang999@gmail.com>
2026-02-18 01:00:41 +0800

    Site updated: 2026-02-18 01:00:40

--- js/main.js
+++ js/main.js
@@ -1,127 +1,85 @@
-// 监听滚动事件
-function listenScroll(callback) {
-  // eslint-disable-next-line no-undef
-  const dbc = new Debouncer(callback);
-  window.addEventListener('scroll', dbc, false);
-  dbc.handleEvent();
-}
+/* main function */
+import initUtils from "./utils.js";
+import initTyped from "./plugins/typed.js";
+import initModeToggle from "./tools/lightDarkSwitch.js";
+import initLazyLoad from "./layouts/lazyload.js";
+import initScrollTopBottom from "./tools/scrollTopBottom.js";
+import initLocalSearch from "./tools/localSearch.js";
+import initCopyCode from "./tools/codeBlock.js";
+import initBookmarkNav from "./layouts/bookmarkNav.js";
 
-// 滚动到指定元素
-function scrollToElement(target, offset) {
-  var scroll_offset = $(target).offset();
-  $('body,html').animate({
-    scrollTop: scroll_offset.top + (offset || 0),
-    easing   : 'swing'
-  });
-}
-
-// 顶部菜单的监听事件
-function navbarScrollEvent() {
-  var navbar = $('#navbar');
-  var submenu = $('#navbar .dropdown-menu');
-  if (navbar.offset().top > 0) {
-    navbar.addClass('navbar-custom');
-    navbar.removeClass('navbar-dark');
-    submenu.addClass('navbar-custom');
-    submenu.removeClass('navbar-dark');
-  }
-  listenScroll(function() {
-    navbar[navbar.offset().top > 50 ? 'addClass' : 'removeClass']('top-nav-collapse');
-    submenu[navbar.offset().top > 50 ? 'addClass' : 'removeClass']('dropdown-collapse');
-    if (navbar.offset().top > 0) {
-      navbar.addClass('navbar-custom');
-      navbar.removeClass('navbar-dark');
-      submenu.addClass('navbar-custom');
-      submenu.removeClass('navbar-dark');
+export const main = {
+  themeInfo: {
+    theme: `Redefine v${theme.version}`,
+    author: "EvanNotFound",
+    repository: "https://github.com/EvanNotFound/hexo-theme-redefine",
+  },
+  localStorageKey: "REDEFINE-THEME-STATUS",
+  styleStatus: {
+    isExpandPageWidth: false,
+    isDark: theme.colors.default_mode && theme.colors.default_mode === "dark",
+    fontSizeLevel: 0,
+    isOpenPageAside: true,
+  },
+  printThemeInfo: () => {
+    console.log(
+      `      ______ __  __  ______  __    __  ______                       \r\n     \/\\__  _\/\\ \\_\\ \\\/\\  ___\\\/\\ \"-.\/  \\\/\\  ___\\                      \r\n     \\\/_\/\\ \\\\ \\  __ \\ \\  __\\\\ \\ \\-.\/\\ \\ \\  __\\                      \r\n        \\ \\_\\\\ \\_\\ \\_\\ \\_____\\ \\_\\ \\ \\_\\ \\_____\\                    \r\n         \\\/_\/ \\\/_\/\\\/_\/\\\/_____\/\\\/_\/  \\\/_\/\\\/_____\/                    \r\n                                                               \r\n ______  ______  _____   ______  ______ __  __   __  ______    \r\n\/\\  == \\\/\\  ___\\\/\\  __-.\/\\  ___\\\/\\  ___\/\\ \\\/\\ \"-.\\ \\\/\\  ___\\   \r\n\\ \\  __<\\ \\  __\\\\ \\ \\\/\\ \\ \\  __\\\\ \\  __\\ \\ \\ \\ \\-.  \\ \\  __\\   \r\n \\ \\_\\ \\_\\ \\_____\\ \\____-\\ \\_____\\ \\_\\  \\ \\_\\ \\_\\\\\"\\_\\ \\_____\\ \r\n  \\\/_\/ \/_\/\\\/_____\/\\\/____\/ \\\/_____\/\\\/_\/   \\\/_\/\\\/_\/ \\\/_\/\\\/_____\/\r\n                                                               \r\n  Github: https:\/\/github.com\/EvanNotFound\/hexo-theme-redefine`,
+    ); // console log message
+  },
+  setStyleStatus: () => {
+    localStorage.setItem(
+      main.localStorageKey,
+      JSON.stringify(main.styleStatus),
+    );
+  },
+  getStyleStatus: () => {
+    let temp = localStorage.getItem(main.localStorageKey);
+    if (temp) {
+      temp = JSON.parse(temp);
+      for (let key in main.styleStatus) {
+        main.styleStatus[key] = temp[key];
+      }
+      return temp;
     } else {
-      navbar.addClass('navbar-dark');
-      submenu.removeClass('navbar-dark');
+      return null;
     }
-  });
-  $('#navbar-toggler-btn').on('click', function() {
-    $('.animated-icon').toggleClass('open');
-    $('#navbar').toggleClass('navbar-col-show');
-  });
-}
-
-// 头图视差的监听事件
-function parallaxEvent() {
-  var target = $('#background[parallax="true"]');
-  var parallax = function() {
-    var oVal = $(window).scrollTop() / 5;
-    var offset = parseInt($('#board').css('margin-top'), 0);
-    var max = 96 + offset;
-    if (oVal > max) {
-      oVal = max;
+  },
+  refresh: () => {
+    initUtils();
+    initModeToggle();
+    initScrollTopBottom();
+    initBookmarkNav();
+    
+    if (
+      theme.home_banner.subtitle.text.length !== 0 &&
+      location.pathname === config.root
+    ) {
+      initTyped("subtitle");
     }
-    target.css({
-      transform          : 'translate3d(0,' + oVal + 'px,0)',
-      '-webkit-transform': 'translate3d(0,' + oVal + 'px,0)',
-      '-ms-transform'    : 'translate3d(0,' + oVal + 'px,0)',
-      '-o-transform'     : 'translate3d(0,' + oVal + 'px,0)'
-    });
 
-    var toc = $('#toc');
-    if (toc) {
-      $('#toc-ctn').css({
-        'padding-top': oVal + 'px'
-      });
+    if (theme.navbar.search.enable === true) {
+      initLocalSearch();
     }
-  };
-  if (target.length > 0) {
-    listenScroll(parallax);
-  }
+
+    if (theme.articles.code_block.copy === true) {
+      initCopyCode();
+    }
+
+    if (theme.articles.lazyload === true) {
+      initLazyLoad();
+    }
+  },
+};
+
+export function initMain() {
+  main.printThemeInfo();
+  main.refresh();
 }
 
-// 向下滚动箭头的监听事件
-function scrollDownArrowEvent() {
-  $('.scroll-down-bar').on('click', function() {
-    scrollToElement('#board', -$('#navbar').height());
-  });
-}
+document.addEventListener("DOMContentLoaded", initMain);
 
-// 向顶部滚动箭头的监听事件
-function scrollTopArrowEvent() {
-  var topArrow = $('#scroll-top-button');
-  if (!topArrow) {
-    return;
-  }
-  var posDisplay = false;
-  var scrollDisplay = false;
-  // 位置
-  var setTopArrowPos = function() {
-    var boardRight = document.getElementById('board').getClientRects()[0].right;
-    var bodyWidth = document.body.offsetWidth;
-    var right = bodyWidth - boardRight;
-    posDisplay = right >= 50;
-    topArrow.css({
-      'bottom': posDisplay && scrollDisplay ? '20px' : '-60px',
-      'right' : right - 64 + 'px'
-    });
-  };
-  setTopArrowPos();
-  $(window).resize(setTopArrowPos);
-  // 显示
-  var headerHeight = $('#board').offset().top;
-  listenScroll(function() {
-    var scrollHeight = document.body.scrollTop + document.documentElement.scrollTop;
-    scrollDisplay = scrollHeight >= headerHeight;
-    topArrow.css({
-      'bottom': posDisplay && scrollDisplay ? '20px' : '-60px'
-    });
+try {
+  swup.hooks.on("page:view", () => {
+    main.refresh();
   });
-  // 点击
-  topArrow.on('click', function() {
-    $('body,html').animate({
-      scrollTop: 0,
-      easing   : 'swing'
-    });
-  });
-}
-
-$(document).ready(function() {
-  navbarScrollEvent();
-  parallaxEvent();
-  scrollDownArrowEvent();
-  scrollTopArrowEvent();
-});
+} catch (e) {}
